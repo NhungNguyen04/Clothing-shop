43b74898fce34ed4669711bbaaf0b8ec76e8b78d
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2025-02-15 13:14:39 +0700

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 import './index.css'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Product from './pages/Product'
-import Cart from './pages/Cart'
-import Login from './pages/Login'
-import PlaceOrder from './pages/PlaceOrder'
-import Orders from './pages/Orders'
 import App from './App.jsx'
-
-const routes = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/collection", element: <Collection /> },
-      { path: "/about", element: <About /> },
-      { path: "/contact", element: <Contact /> },
-      { path: "/product/:productId", element: <Product /> },
-      { path: "/cart", element: <Cart /> },
-      { path: "/login", element: <Login /> },
-      { path: "/place-order", element: <PlaceOrder /> },
-      { path: "/orders", element: <Orders /> },
-    ],
-  },
-]);
+import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={routes} />
-  </StrictMode>
+  <BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
 )
