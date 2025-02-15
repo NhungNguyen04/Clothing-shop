3226e913fe2ed3a4ea18023fc411e49de9e4342f
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2025-02-15 12:58:47 +0700

    navbar & hero

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,23 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/collection" element={<Collection/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="/contact" element={<Contact/>}></Route>
-        <Route path="/product/:productId" element={<Product/>}></Route>
-        <Route path="/cart" element={<Cart/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/place-order" element={<PlaceOrder/>}></Route>
-        <Route path="/orders" element={<Orders/>}></Route>
-      </Routes>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/collection" element={<Collection/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/product/:productId" element={<Product/>}></Route>
+          <Route path="/cart" element={<Cart/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/place-order" element={<PlaceOrder/>}></Route>
+          <Route path="/orders" element={<Orders/>}></Route>
+        </Routes>
     </div>
   )
 }
