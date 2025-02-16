29ab41453f693fb570e3751bfa72b6380e628bca
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2025-02-16 11:17:25 +0700

    search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '@/assets/assets'
 import { Link, NavLink } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
   const [visible, setVisible] = React.useState(false)
+  const { setShowSearch } = useContext(ShopContext);
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -29,7 +31,7 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className='flex items-center gap-6'>
-        <img src={assets.search_icon} alt="" className='w-5 cursor-pointer' />
+        <img onClick={() => setShowSearch(true)} src={assets.search_icon} alt="" className='w-5 cursor-pointer' />
         <div className='group relative'>
           <img src={assets.profile_icon} alt="" className='w-5 cursor-pointer'/>
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 function Collection() {
 
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [ showFilter, setShowFilter ] = useState(true)
   const [ filterProducts, setFilterProducts ] = useState([]);
   const [ category, setCategory ] = useState([]);
@@ -31,6 +31,10 @@ function Collection() {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    } 
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category))
@@ -61,7 +65,7 @@ function Collection() {
 
   useEffect(() => {
     applyFilter()
-  }, [category, subCategory])
+  }, [category, subCategory, search, showSearch])
 
   useEffect(() => {
     sortProducts()
